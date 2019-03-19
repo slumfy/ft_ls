73ecacfd602216855d1749c3bb37d8bcb7e44234
Robert VALENTIN <rvalenti@e2r9p1.42.fr>
2019-03-19 15:44:11 +0100

    define pour les bytes field

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 12:13:06 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/03/19 14:47:02 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/03/19 15:43:49 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,13 @@
 # include <grp.h>
 # include "libft/libft.h"
 # include <limits.h>
+
+#define OPT_l 1
+#define OPT_R 2
+#define OPT_a 4
+#define OPT_r 8
+#define OPT_t 16
+
 
 typedef	struct	s_dir
 {
