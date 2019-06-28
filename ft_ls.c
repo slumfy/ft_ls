57c70f9d1d92b383501f27f6698db69afa7e2338
Robert Valentin <Slumfy@macs-MacBook-Air.local>
2019-06-28 12:36:09 +0200

    test

--- ft_ls.c
+++ ft_ls.c
@@ -18,13 +18,6 @@ void ft_ls(t_data *data, char *current)
 	t_file			*tmp;
 
 	tmp= NULL;
-	if (data->dir_list)
-	{
-		tmp = data->dir_list->next;
-		free(data->dir_list);
-		data->dir_list = tmp;
-	}
-	tmp = data->list;
 	if ((data->dir.curdir = opendir(current)))
 	{
 		while ((file = readdir(data->dir.curdir)) != NULL)
@@ -34,11 +27,22 @@ void ft_ls(t_data *data, char *current)
 		if (data->fmt & OPT_r)
 			rev_list(&data->list);
 		print_list(data, data->list);
-		if (data->fmt & OPT_R)
-			deleteif_list(&data->list, data);
-		free_list(&data->list);
 		closedir(data->dir.curdir);
 	}
 	else
-	printf("ls:%s:%s\n",current , strerror(errno));
+	{
+		printf("ls:%s:%s\n",current , strerror(errno));
+	}
+	if (data->dir_list)
+	{
+		tmp = data->dir_list->next;
+		free(data->dir_list);
+		data->dir_list = tmp;
+	}
+	if (data->fmt & OPT_R)
+	{
+		deleteif_list(&data->list, data);
+		print_list(data, data->dir_list);
+	}
+	free_list(&data->list);
 }
